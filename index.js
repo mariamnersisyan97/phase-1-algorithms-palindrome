50d9191b0a925be5bcc9a31fa83b0c19098da73f
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-23 21:53:21 -0400

    commit tree 63399404b5491b5c770e0ae8ba3b694b954bb682

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   if(word === word.split('').reverse().join('')){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  // create if/else statement that takes in param & use relational operator
+  // split the string, reverse it, then join it
+  // return result
 */
 
 /*
   Add written explanation of your solution here
+  My function takes in a param of word. I created an if/else statement that compares the conditions with the word. The string is split into an array of substrings, reversed, then joined & compared to the original string. If the parts of the string are the same, then it is effectively a palindrome and therefore returns true.
 */
 
 // You can run `node index.js` to view these console logs
